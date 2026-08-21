baba199e2d645d1f6fda853c5ab9694f197b4ffe
faiz-ux-12 <2550091058@student.unjani.ac.id>
2026-08-21 16:30:54 +0700

    Update Spotify track URI and ID

--- script.js
+++ script.js
@@ -224,8 +224,8 @@ fetch('foto.json')
    pengguna, jadi browser lebih mengizinkan audio jalan).
    Kalau API gagal dimuat, otomatis jatuh ke player biasa.
    ========================================================= */
-const SPOTIFY_TRACK_URI = 'spotify:track:2yhAtx8uyIKRnxLX3ftu8I';
-const SPOTIFY_TRACK_ID  = '2yhAtx8uyIKRnxLX3ftu8I';
+const SPOTIFY_TRACK_URI = 'spotify:track:2rbDhOo9Fh61Bbu23T2qCk';
+const SPOTIFY_TRACK_ID = '2rbDhOo9Fh61Bbu23T2qCk';
 let spotifyController = null;
 
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
